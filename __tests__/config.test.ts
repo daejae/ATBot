27c410b15b8af75d69a7f5003e029d750a001f0c
daejae <daejae135@daum.net>
2024-04-28 04:10:51 +0900

    add .env property(stock info)

--- __tests__/config.test.ts
+++ __tests__/config.test.ts
@@ -5,4 +5,7 @@ test('설정 속성은 비어 있지 않은 값이어야 함', () => {
   expect(config.accountNumber).toBeTruthy();
   expect(config.appKey).toBeTruthy();
   expect(config.appSecret).toBeTruthy();
+  expect(config.stockName).toBeTruthy();
+  expect(config.stockMarket).toBeTruthy();
+  expect(config.stockBuyQty).toBeTruthy();
 });
--- src/config.ts
+++ src/config.ts
@@ -6,6 +6,11 @@ interface Config {
   accountNumber: string;
   appKey: string;
   appSecret: string;
+
+  // 종목
+  stockName :string;
+  stockMarket : string;
+  stockBuyQty : string;
 }
 
 const config: Config = {
@@ -13,6 +18,10 @@ const config: Config = {
   accountNumber: process.env.ACCOUNT_NUMBER,
   appKey: process.env.APP_KEY,
   appSecret: process.env.APP_SECRET,
+
+  stockName : process.env.STOCK_NAME,
+  stockMarket : process.env.SOCKET_MARKET,
+  stockBuyQty : process.env.STOCK_BUY_QTY,
 };
 
 export default config;
