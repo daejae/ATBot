a10424e087511a505cbc160a520fdf6eb682fbc4
daejae <daejae135@daum.net>
2024-04-28 00:25:49 +0900

    add prismaClient

---
+++ __tests__/prismaClient.test.ts
@@ -0,0 +1,15 @@
+import prisma from "../src/prismaClient.js"
+
+describe("Prisma Clien 테스트", ()=>{
+  beforeAll(async ()=>{
+    await prisma.$connect();
+  });
+
+  beforeAll(async ()=>{
+    await prisma.$disconnect();
+  });
+
+  test("case 1", ()=>{
+
+  })
+})
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+// // // 프로그램 진입점.
+// // (async () => {
+// //   setInterval(()=>{
+// //     console.log(123123);
+// //   }, 1000)
+// // })();
+
+// import prisma from "./prismaClient.js";
+
+// console.log(prisma);
